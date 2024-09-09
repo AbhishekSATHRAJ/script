464e6a8bdea53576551184a52bc002179c2cf961
AbhishekSATHRAJ <sathrajabhishek78@gmail.com>
2024-09-09 10:48:23 +0530

    Add files via upload
    modified

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// // Retrieve the necessary elements
+// var usernameInput = document.getElementById("input");
+// var saveButton = document.getElementById("Generetor");
+
+// // Add event listener to the button
+// Generetor.addEventListener("click", function () {
+//   // Retrieve the value from the text field
+//   var username = usernameInput.value;
+
+//   // Display the username under the profile picture
+//   var usernameDisplay = document.createElement(".phones");
+//   usernameDisplay.innerHTML = username;
+//   usernameDisplay.classList.add("username");
+//   document.querySelector(".container").appendChild(usernameDisplay);
+// });
+
+
+
+// var imageSources = ["https://cdn1.smartprix.com/rx-iaRsF1EHH-w420-h420/iqoo-neo-9-pro-5g-8g.webp,https://cdn1.smartprix.com/rx-ipdMi0Fy9-w420-h420/iqoo-z9s-pro-5g.webp,https://cdn1.smartprix.com/rx-iuRWmtC7U-w420-h420/motorola-edge-50-pro.webp"]
+
+//     imageSources.forEach(element => {
+//         var img = document.createElement("img");
+//         img.width = '300';
+//         img.height = '300';
+//         img.src = element;
+//         document.getElementById("content").appendChild(img)
+//     });
+
+
+
+  
+
